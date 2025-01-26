42fc74c28d59c7b87224d686128f9d98ccf1da33
JPCabral04 <jpqcabral04@gmail.com>
2025-01-26 15:10:35 -0300

    ✨ enhance Projects component with SCSS styles and additional project entries

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -36,6 +36,62 @@ export class ProjectsComponent {
           href: 'https://vidafullstack.com.br',
         }
       ]
-    }
+    },
+    {
+      src: 'assets/img/projects/lista-de-tarefas.png',
+      alt : 'Projeto 1',
+      title: 'Vida Full Stack',
+      width: '100px',
+      height: '51px',
+      description: '',
+      links : [
+        {
+          name : 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/lista-de-tarefas.png',
+      alt : 'Projeto 1',
+      title: 'Vida Full Stack',
+      width: '100px',
+      height: '51px',
+      description: '',
+      links : [
+        {
+          name : 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/lista-de-tarefas.png',
+      alt : 'Projeto 1',
+      title: 'Vida Full Stack',
+      width: '100px',
+      height: '51px',
+      description: '',
+      links : [
+        {
+          name : 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/lista-de-tarefas.png',
+      alt : 'Projeto 1',
+      title: 'Vida Full Stack',
+      width: '100px',
+      height: '51px',
+      description: '',
+      links : [
+        {
+          name : 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        }
+      ]
+    },
   ])
 }
