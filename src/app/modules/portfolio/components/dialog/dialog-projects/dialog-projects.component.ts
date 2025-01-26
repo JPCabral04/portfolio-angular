88741de2e6d792d193ea51d1578203b6653ea109
JPCabral04 <jpqcabral04@gmail.com>
2025-01-26 15:29:36 -0300

    ✨ implement dialog for project details with MatDialog integration

---
+++ src/app/modules/portfolio/components/dialog/dialog-projects/dialog-projects.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject, OnInit, signal } from '@angular/core';
+
+//Interface
+import { IProjects } from '../../../interface/IProjects.interface';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-projects',
+  imports: [],
+  templateUrl: './dialog-projects.component.html',
+  styleUrl: './dialog-projects.component.scss'
+})
+export class DialogProjectsComponent implements OnInit{
+
+  constructor(@Inject(MAT_DIALOG_DATA) private _data : IProjects){
+    
+  }
+
+  public getData = signal<IProjects | null>(null);
+
+  ngOnInit(): void {
+    this.getData.set(this._data);
+  }
+}
--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,13 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+
+//Interface
 import { IProjects } from '../../interface/IProjects.interface';
+
+//Material
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+
+//Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+  private dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/vfull.png',
@@ -94,4 +106,11 @@ export class ProjectsComponent {
       ]
     },
   ])
+
+  public openDialog(data: IProjects){
+    this.dialog.open(DialogProjectsComponent,{
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 }
